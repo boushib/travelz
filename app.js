0d5a0cb9429f75134a7b0caf43e9ee0023d121ad
boushib <boushib01@gmail.com>
2020-03-01 22:29:08 +0100

    creating a basic express app

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const fs = require('fs')
+require('dotenv').config()
+
+const app = express()
+app.use(express.json())
+
+const locations = fs.readFileSync(`${__dirname}/data/locations.json`, 'utf-8')
+const locationsObj = JSON.parse(locations)
+
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'welcome to travelz!'})
+})
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).send({
+    status: "success",
+    results: locationsObj.length,
+    data: {locations: locationsObj}
+  })
+})
+app.post('/api/v1/tours', (req, res) => {
+  const id = locationsObj[locationsObj.length - 1]._id + 1
+  const location = {_id: id, ...req.body}
+  locationsObj.push(location)
+  fs.writeFile(`${__dirname}/data/locations.json`, JSON.stringify(locationsObj), err => {
+    if(err) return console.log(err)
+    res.status(201).json({message: 'Location successfuly created!', data: location})
+  })
+})
+
+const port = process.env.PORT
+app.listen(port, ()=> {
+  console.log(`app running on port ${port}`)
+})
