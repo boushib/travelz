8902fb98bac8dd2b3cf033fdff4535b7bee5c5ac
boushib <boushib01@gmail.com>
2020-03-04 16:36:46 +0100

    adding mongoose and mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-require('dotenv').config()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(con => console.log("DB connected successfuly!"))
+  .catch(err => console.log("ERROR: ", err))
+
 const app = require('./app')
-console.log(process.env.NODE_ENV)
 const port = process.env.PORT
+
 app.listen(port, () => {
   console.log(`app running on port ${port}`)
 })
